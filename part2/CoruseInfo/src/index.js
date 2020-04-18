c93b30db3d5e79f4d04fb542e31dd5eef9a8c3d7
Bradley Bond <bradleybond766@gmail.com>
2020-04-17 20:38:06 -0600

    Finished exercise 2.5

--- part2/CoruseInfo/src/index.js
+++ part2/CoruseInfo/src/index.js
@@ -1,62 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Course = (props) =>{
-	console.log(props.parts)
-	return (
-		<div>
-		<Header course={props.name}/>
-      	<Content course={props.parts}/>
-      	<Total course={props.parts}/>
-      	</div>
-		)
-}
-
-const Header = (props) =>{
-	return(
-		<div>
-			<h1>
-				{props.course}
-			</h1>
-		</div>
-	)
-}
-
-const Total = ({course}) =>{
-	let total = 0
-	let totalArray = []
-	let message1 = "Total of "
-	let message2 = " exercises"
-	totalArray = course.map(course =>(
-		course.exercises			
-	))
-	total = totalArray.reduce((s,p) => (s+p))
-	return(
-	<div>
-			<p>
-				<b>{message1} {total} {message2}</b>
-			</p>
-	</div>
-	)
-}
-
-const Part = (props) =>{
-	return(
-		<div>
-			<p>
-	 			{props.part} {props.exercise}
-			</p>
-		</div>
-	)
-}
-
-const Content = ({course}) =>{
-	return course.map(course =>(
-		<div key = {course.id}>
-			<Part part = {course.name} exercise = {course.exercises}/>
-		</div>
-	));
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
