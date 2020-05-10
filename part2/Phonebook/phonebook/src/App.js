97d836ff9cb03bbe11ada034f84a597dfaaa4858
Bradley Bond <bradleybond766@gmail.com>
2020-05-10 15:52:22 -0600

    Completed exercises 2.19

--- part2/Phonebook/phonebook/src/App.js
+++ part2/Phonebook/phonebook/src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect} from 'react'
 import RenderPhoneBook from './Components/RenderPhoneBook'
 import Filter from './Components/Filter'
 import personsService from './Services/persons'
+import './index.css'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [changeMessage, setChangeMessage] = useState(null)
 
   var nameToShow = persons
 
@@ -39,6 +41,12 @@ const App = () => {
       return
     }
   }
+
+  setChangeMessage(`Added ${nameObject.name}`)
+  setTimeout(() =>{
+    setChangeMessage(null)
+  },5000)
+
     personsService
         .create(nameObject)
         .then(response => {
@@ -67,9 +75,22 @@ const deletePerson = (props) => {
   }
 }
 
+const Notification = ({message}) =>{
+  if (message === null){
+    return null
+  }
+
+  return(
+    <div className = "change">
+      {message}
+    </div>
+    )
+}
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message = {changeMessage}/>
       <form>
         <div> Filter: <input
           onChange = {filterPerson}/>
